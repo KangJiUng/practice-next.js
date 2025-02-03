7e50ac40c03361165769e8dc5ec76f493ee1d98c
KangJiUng <woong236406@gmail.com>
2025-02-03 15:25:42 +0900

    CHAPTER3_Next.js기초와내장컴포넌트

---
+++ PART1/my-first-next-app/pages/contact.js
@@ -0,0 +1,13 @@
+function ContactPage() {
+  return (
+    <div>
+      <ul>
+        <li> Email: asd@example.com</li>
+        <li> Twitter: @asd</li>
+        <li> Instagram: asd</li>
+      </ul>
+    </div>
+  );
+}
+
+export default ContactPage;
