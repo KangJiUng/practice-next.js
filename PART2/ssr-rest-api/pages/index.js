58e26145d5c5a24b0f0bce37ab84580f4b4c7056
KangJiUng <woong236406@gmail.com>
2025-02-04 19:58:16 +0900

    CHAPTER4_코드구성과데이터불러오기

---
+++ PART2/ssr-rest-api/pages/index.js
@@ -0,0 +1,29 @@
+import { useEffect } from "react";
+import Link from "next/link";
+import axios from "axios";
+
+export async function getServerSideProps() {
+  const usersReq = await axios.get("https://api.rwnjs.com/04/users");
+
+  return {
+    props: {
+      users: usersReq.data,
+    },
+  };
+}
+
+function HomePage({ users }) {
+  return (
+    <ul>
+      {users.map((user) => (
+        <li key={user.id}>
+          <Link href={`/users/${user.username}`} passHref>
+            {user.username}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default HomePage;
